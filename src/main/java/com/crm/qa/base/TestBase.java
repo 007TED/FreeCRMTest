3f1bb5ec468773d3066bfe66074b03521611ebea
Gavish Sethi <gavish.sethi7@gmail.com>
2023-04-11 07:06:46 -0400

    Update TestBase.java
    changing prop file path

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -28,8 +28,7 @@ public class TestBase {
 		
 		try {
 			prop = new Properties();
-			FileInputStream ip = new FileInputStream("/Users/gavish/eclipse-workspace/FreeCRMTest/src/main/"
-					+ "java/com/crm/qa/config/config.properties");
+			FileInputStream ip = new FileInputStream("src/main/java/com/crm/qa/config/config.properties");
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
